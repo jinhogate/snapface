260684637d7257ccbbfba6f8cd33c03dbca6854f
GBEGNON <kossijorjinho.gbegnon@gmail.com>
2025-07-16 20:09:47 +0200

    [25]: Attribut binding - ajout police et  css

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,11 +11,14 @@ export class FaceSnapComponent implements OnInit {
   description!: string;
   createdAt!: Date;
   snaps!: number;
+  image!: string;
 
   ngOnInit(): void {
     this.title = "Archibald";
     this.description = "Mon meilleur ami depuis toujours !";
     this.createdAt = new Date();
     this.snaps = 5;
+    this.image =
+      "https://images.pexels.com/photos/2113709/pexels-photo-2113709.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
   }
 }
